482760f31ee84dd34c4f84d47bfc3c4dc6bc0094
Igor Fernandes de Queiroz <igor.queiroz@cwi.com.br>
2020-01-30 15:26:40 -0300

    Adding two other solutions

--- src/br/com/eaglequeiroz/projecteuler/_06_differenceBetweenSumSquares.java
+++ src/br/com/eaglequeiroz/projecteuler/_06_differenceBetweenSumSquares.java
@@ -2,6 +2,12 @@ package br.com.eaglequeiroz.projecteuler;
 
 import java.util.stream.LongStream;
 
+/**
+ *
+ * @author Igor Queiroz
+ * This Algorithm is responsible for calculate the difference between
+ * the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
 public class _06_differenceBetweenSumSquares {
 
     public static void main(String[] args) {
